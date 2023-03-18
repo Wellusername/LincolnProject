487ac4e231f480f91c743a787afb2b1162240698
Wellusername <biao.wang@lincolnuni.ac.nz>
2023-03-19 00:55:33 +1300

    resturcture helper method in qrGeneration

--- controllers/qrGeneration.js
+++ controllers/qrGeneration.js
@@ -1,4 +1,9 @@
 const { mapTermToTermObject } = require("../utils/helper");
+const {
+  checkPattern,
+  formateDate,
+  gs1Measurement,
+} = require("../utils/formater");
 
 exports.generateQRLink = async (req, res, next) => {
   const {
@@ -94,42 +99,4 @@ exports.generateQRLink = async (req, res, next) => {
       message: e.message,
     });
   }
-};
-
-checkPattern = (val, termObj) => {
-  if (typeof termObj.pattern !== "undefined") {
-    var reg = new RegExp(termObj.pattern);
-
-    console.log(val, reg, reg.test(val));
-
-    if (!reg.test(val)) {
-      throw new Error(termObj.id + " is not the right pattern");
-    }
-  }
-};
-
-formateDate = (time) => {
-  time = time.replace(/\D/g, "");
-  time = time.substring(2);
-  return time;
-};
-
-gs1Measurement = (val) => {
-  var ndp = 0;
-  var rv = {};
-  var numeric = "";
-  if (val.indexOf(".") > -1) {
-    var parts = val.split(".");
-    ndp = parts[1].length;
-    numeric = parts[0] + parts[1];
-  } else {
-    numeric = val;
-  }
-  if (numeric.length < 6) {
-    numeric = "0".repeat(6 - numeric.length) + numeric;
-  }
-
-  rv.ndp = ndp;
-  rv.sixdigit = numeric;
-  return rv;
 };
---
+++ utils/formater.js
@@ -0,0 +1,37 @@
+exports.checkPattern = (val, termObj) => {
+  if (typeof termObj.pattern !== "undefined") {
+    var reg = new RegExp(termObj.pattern);
+
+    console.log(val, reg, reg.test(val));
+
+    if (!reg.test(val)) {
+      throw new Error(termObj.id + " is not the right pattern");
+    }
+  }
+};
+
+exports.formateDate = (time) => {
+  time = time.replace(/\D/g, "");
+  time = time.substring(2);
+  return time;
+};
+
+exports.gs1Measurement = (val) => {
+  var ndp = 0;
+  var rv = {};
+  var numeric = "";
+  if (val.indexOf(".") > -1) {
+    var parts = val.split(".");
+    ndp = parts[1].length;
+    numeric = parts[0] + parts[1];
+  } else {
+    numeric = val;
+  }
+  if (numeric.length < 6) {
+    numeric = "0".repeat(6 - numeric.length) + numeric;
+  }
+
+  rv.ndp = ndp;
+  rv.sixdigit = numeric;
+  return rv;
+};
