630b3a6ce5416be931657097daedaa56801a5359
Wellusername <biao.wang@lincolnuni.ac.nz>
2023-03-25 20:13:02 +1300

    display qr code

--- frontend/src/pages/QRcodeGenerationLayout.js
+++ frontend/src/pages/QRcodeGenerationLayout.js
@@ -1,13 +1,28 @@
-import { Grid, TextareaAutosize } from "@mui/material";
+import { Grid, TextareaAutosize, Typography } from "@mui/material";
 import React from "react";
+import QRcodeDisplay from "../components/QRCode/QRcodeDisplay";
 
 function QRcodeGenerationLayout() {
   return (
     <Grid container>
       <Grid item xs={12}>
+        <Typography variant="h5" sx={{ fontWeight: "bold" }} textAlign="center">
+          {" "}
+          URL
+        </Typography>
+      </Grid>
+      <Grid item xs={12}>
         <TextareaAutosize minRows={15} maxRows={20} style={{ width: "100%" }} />
       </Grid>
-      <Grid item xs={12}></Grid>
+      <Grid item xs={12}>
+        <Typography variant="h5" sx={{ fontWeight: "bold" }} textAlign="center">
+          {" "}
+          QR Code
+        </Typography>
+      </Grid>
+      <Grid item xs={12}>
+        <QRcodeDisplay />
+      </Grid>
     </Grid>
   );
 }
