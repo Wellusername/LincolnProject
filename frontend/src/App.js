a71e193307eab9f00d5beeddc91ba2a61c99807a
Wellusername <biao.wang@lincolnuni.ac.nz>
2023-03-26 16:11:20 +1300

    added info state for cature freight detail

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,24 @@
-import "./App.css";
 import { Grid } from "@mui/material";
 import InfoInputLayout from "./pages/InfoInputLayout";
 import QRcodeGenerationLayout from "./pages/QRcodeGenerationLayout";
+import { useState } from "react";
 
 function App() {
+  const [info, setInfo] = useState({});
+
+  const handleInput = (event, id) => {
+    const newInfo = { ...info };
+    newInfo[id] = event.target.value;
+    setInfo(newInfo);
+  };
+
   return (
     <Grid container>
       <Grid item xs={12} md={6}>
-        <InfoInputLayout />
+        <InfoInputLayout handleInput={handleInput} />
       </Grid>
       <Grid item xs={12} md={6}>
-        <QRcodeGenerationLayout />
+        <QRcodeGenerationLayout info={info} />
       </Grid>
     </Grid>
   );
