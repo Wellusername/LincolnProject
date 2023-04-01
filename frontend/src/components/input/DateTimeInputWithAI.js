46936c5d9451521d2b2ef61063c667a95cc52a97
Wellusername <biao.wang@lincolnuni.ac.nz>
2023-04-01 21:24:13 +1300

    added clear for date input

--- frontend/src/components/input/DateTimeInputWithAI.js
+++ frontend/src/components/input/DateTimeInputWithAI.js
@@ -1,7 +1,28 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Grid, TextField, Typography } from "@mui/material";
 
-function DateTimeInputWithAI({ id, label, AI, color, handleInput }) {
+function DateTimeInputWithAI({
+  id,
+  label,
+  AI,
+  color,
+  handleInput,
+  clear,
+  handleClearInput,
+}) {
+  const [text, setText] = useState("");
+
+  useEffect(() => {
+    if (clear) {
+      console.log(clear);
+      setText("");
+      handleClearInput(false);
+    }
+  }, [clear]);
+
+  const handleLocalInput = (e) => {
+    setText(e.value);
+  };
   return (
     <Grid container style={{ backgroundColor: color }}>
       <Grid item xs={5} style={{ display: "flex", alignItems: "center" }}>
@@ -23,7 +44,11 @@ function DateTimeInputWithAI({ id, label, AI, color, handleInput }) {
             borderRadius: "4px",
           }}
           size="small"
-          onChange={(e) => handleInput(e, id)}
+          onChange={(e) => {
+            handleInput(e, id);
+            handleLocalInput(e);
+          }}
+          value={text}
         />
       </Grid>
       <Grid
