0c986fff24300c1ba2daf3d50fcb87e48c291a14
Wellusername <biao.wang@lincolnuni.ac.nz>
2023-04-01 20:23:19 +1300

    check for empty user input

--- frontend/src/layouts/QRcodeGenerationLayout.js
+++ frontend/src/layouts/QRcodeGenerationLayout.js
@@ -1,20 +1,34 @@
 import React, { useState, useEffect } from "react";
-import { Grid, TextareaAutosize, Typography } from "@mui/material";
+import { Button, Grid, TextareaAutosize, Typography } from "@mui/material";
 import QRcodeDisplay from "../components/QRCode/QRcodeDisplay";
 import { getUrlAndQRcode } from "../utils/connectBackend";
 
 function QRcodeGenerationLayout({ info }) {
   const [result, setResult] = useState({});
+  const [generatedURL, setGeneratedURL] = useState(false);
   useEffect(() => {
     try {
-      getUrlAndQRcode(info).then((res) => {
-        console.log(res);
-        setResult(res);
-      });
+      const containEmptyString = (element) => element === "";
+      const val = Object.values(info);
+      if (val.filter(containEmptyString).length != val.length) {
+        getUrlAndQRcode(info).then((res) => {
+          setResult(res);
+        });
+      } else {
+        setResult({ url: "" });
+      }
     } catch (e) {
       console.log(e);
     }
   }, [info]);
+
+  useEffect(() => {
+    if (result != undefined && result != null && result.url != "") {
+      setGeneratedURL(true);
+    } else {
+      setGeneratedURL(false);
+    }
+  }, [result]);
 
   return (
     <Grid container>
@@ -40,6 +54,30 @@ function QRcodeGenerationLayout({ info }) {
       </Grid>
       <Grid item xs={12}>
         <QRcodeDisplay qrcode={result.image} />
+      </Grid>
+      <Grid
+        item
+        container
+        xs={12}
+        justifyContent={"space-between"}
+        style={{ marginTop: "30px" }}
+      >
+        <Grid item xs={2} />
+        <Grid item xs={2}>
+          <Button variant="outlined" style={{ minWidth: "100%" }}>
+            Clear
+          </Button>
+        </Grid>
+        <Grid item xs={2}>
+          <Button
+            variant="outlined"
+            style={{ minWidth: "100%" }}
+            disabled={!generatedURL}
+          >
+            Generate
+          </Button>
+        </Grid>
+        <Grid item xs={2} />
       </Grid>
     </Grid>
   );
