233374574cf0122edc8b3cbe9903b5e09bf9557c
Wellusername <biao.wang@lincolnuni.ac.nz>
2023-04-24 21:18:27 +1200

    format

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,9 @@ require("dotenv").config({ path: "./config.env" });
 const express = require("express");
 const cors = require("cors");
 const app = express();
+const swaggerJSDoc = require("swagger-jsdoc");
+const path = require("path");
+const swaggerui = require("swagger-ui-express");
 
 app.use(cors());
 app.use(express.json()); // support json encoded boies
@@ -13,6 +16,28 @@ const PORT = process.env.PORT || 5000;
 const server = app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
+
+const options = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "Freight Data Processor",
+      version: "1.0.0",
+      description: "My API description",
+    },
+    servers: [
+      {
+        url: "http://localhost:5000/",
+        description: "Development server",
+      },
+    ],
+  },
+  apis: [path.join(__dirname, "/routes/*.js")],
+};
+
+const swaggerSpec = swaggerJSDoc(options);
+
+app.use("/api-docs", swaggerui.serve, swaggerui.setup(swaggerSpec));
 
 app.use("/api/generation", require("./routes/qrGeneration"));
 app.use("/api/event", require("./routes/epicEvent"));
